8275b44e5397d5278d9a98166d2e9eb3a6124a3a
Elias Röbl <31956958+Morser999@users.noreply.github.com>
2018-03-01 13:36:39 +0100

    Add files via upload

--- caravan.cpp
+++ caravan.cpp
@@ -10,12 +10,34 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include "pack_animal.h"
+#include "caravan.h"
+
+struct Node{
+  PackAnimal data;
+  struct Node* next;
+};
+
+struct CaravanImplementation{
+  struct Node* head;
+  struct Node* tail;
+  int length = 0;
+};
+
 Caravan new_caravan()
 {
+    Caravan new_caravan = (Caravan)malloc(sizeof(struct CaravanImplementation));
+    return new_caravan;
 }
 
 int get_length(Caravan caravan)
 {
+  if(caravan->head->data != NULL){
+    return 1;
+  }
+  return 0;
 }
 
 void delete_caravan(Caravan caravan)
@@ -24,6 +46,18 @@ void delete_caravan(Caravan caravan)
 
 void add_pack_animal(Caravan caravan, PackAnimal animal)
 {
+  if(caravan->head->data == NULL){
+    caravan->head->data = animal;
+  }
+  else{
+    Node* curr = caravan->head;
+    while(curr->next->data == NULL){
+      curr = curr->next;
+    }
+    curr->next->data = animal;
+  }
+  caravan->length += 1;
+
 }
 
 void remove_pack_animal(Caravan caravan, PackAnimal animal)
@@ -32,6 +66,7 @@ void remove_pack_animal(Caravan caravan, PackAnimal animal)
 
 int get_caravan_load(Caravan caravan)
 {
+  return 0;
 }
 
 void unload(Caravan caravan)
@@ -40,4 +75,6 @@ void unload(Caravan caravan)
 
 int get_caravan_speed(Caravan caravan)
 {
+  return 0;
 }
+void optimize_load(Caravan caravan){}
